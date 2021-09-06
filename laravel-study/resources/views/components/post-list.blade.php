a57049d7b841ed68ea66d890694e281670175574
JOWONCHAN <dnjscks0709@naver.com>
2021-09-06 19:18:58 +0900

    2021/09/06

---
+++ laravel-study/resources/views/components/post-list.blade.php
@@ -0,0 +1,39 @@
+<div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+    {{-- @foreach ($posts as $post)
+        <div class="my-2">
+            <p>
+                {{ $post->content }}
+            </p>
+        </div>
+    @endforeach --}}
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+</div>
---
+++ laravel-study/resources/views/layouts/hello.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>hello</h1>
+@endsection
+
+@section('content2')
+    <h2>hello2</h2>
+@endsection
